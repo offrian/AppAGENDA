a67319243956f7030aee6e2b0ea66985ea0d87ef
offrian <rianramosnacimento1018@gmail.com>
2023-11-19 18:27:52 -0300

    Cadastro de Cliente

--- server.js
+++ server.js
@@ -11,14 +11,55 @@ app.use(bodyParser.json());
 
 const clientAppointments = [];
 const professionalAppointments = [];
+const users = []; // Array para armazenar dados dos usuários
+
+app.post('/api/cadastrar-usuario', (req, res) => {
+  const { username, password, role } = req.body;
+
+  // Verifica se o nome de usuário já existe
+  if (users.some(user => user.username === username)) {
+    return res.status(400).json({ error: 'Nome de usuário já cadastrado.' });
+  }
+
+  const newUser = { id: uuidv4(), username, password, role };
+
+  // Adiciona o novo usuário ao array de usuários
+  users.push(newUser);
+
+  console.log('Novo usuário cadastrado:', newUser);
+  res.status(200).json({ message: 'Usuário cadastrado com sucesso!' });
+});
+
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Procura o usuário com o nome de usuário e senha correspondentes
+  const user = users.find(u => u.username === username && u.password === password);
+
+  if (user) {
+    console.log('Login bem-sucedido:', user);
+    res.status(200).json({ message: 'Login bem-sucedido!', user });
+  } else {
+    res.status(401).json({ error: 'Nome de usuário ou senha incorretos.' });
+  }
+});
 
 app.post('/api/agendamentos-cliente', (req, res) => {
-  const novoAgendamentoCliente = { ...req.body, id: uuidv4() }; 
+  const novoAgendamentoCliente = { ...req.body, id: uuidv4() };
   clientAppointments.push(novoAgendamentoCliente);
 
   console.log('Agendamentos do cliente atualizados:', clientAppointments);
 
   res.status(200).json({ message: 'Agendamento do cliente recebido com sucesso!' });
+});
+
+app.post('/api/agendamentos-profissional', (req, res) => {
+  const novoAgendamentoProfissional = { ...req.body, id: uuidv4() };
+  professionalAppointments.push(novoAgendamentoProfissional);
+
+  console.log('Agendamentos do profissional atualizados:', professionalAppointments);
+
+  res.status(200).json({ message: 'Agendamento do profissional recebido com sucesso!' });
 });
 
 app.get('/api/agendamentos-profissional', (req, res) => {
--- src/components/LoginScreenCliente.js
+++ src/components/LoginScreenCliente.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 import './LoginScreen.css';
 
 const LoginScreen = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [loginSuccess, setLoginSuccess] = useState(false);
+  const [showSignupModal, setShowSignupModal] = useState(false);
+  const [signupUsername, setSignupUsername] = useState('');
+  const [signupPassword, setSignupPassword] = useState('');
 
   const history = useHistory();
 
@@ -22,13 +26,42 @@ const LoginScreen = () => {
     });
   };
 
-  const handleLogin = () => {
-    if (username === 'cliente' && password === 'cliente') {
-      setLoginSuccess(true);
-      showNotification('Login bem-sucedido!', 'success');
-    } else {
-      setLoginSuccess(false);
-      showNotification('Nome de usuário ou senha incorretos', 'error');
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:3005/api/login', {
+        username,
+        password,
+      });
+
+      if (response.data.message === 'Login bem-sucedido!') {
+        setLoginSuccess(true);
+        showNotification('Login bem-sucedido!', 'success');
+      } else {
+        setLoginSuccess(false);
+        showNotification('Nome de usuário ou senha incorretos', 'error');
+      }
+    } catch (error) {
+      console.error('Erro ao realizar login:', error);
+      showNotification('Erro ao realizar login', 'error');
+    }
+  };
+
+  const handleSignup = async () => {
+    try {
+      const response = await axios.post('http://localhost:3005/api/cadastrar-usuario', {
+        username: signupUsername,
+        password: signupPassword,
+      });
+
+      if (response.data.message === 'Usuário cadastrado com sucesso!') {
+        showNotification('Cadastro realizado com sucesso!', 'success');
+        setShowSignupModal(false);
+      } else {
+        showNotification('Erro ao cadastrar. Tente novamente.', 'error');
+      }
+    } catch (error) {
+      console.error('Erro ao cadastrar:', error);
+      showNotification('Erro ao cadastrar. Tente novamente.', 'error');
     }
   };
 
@@ -64,9 +97,38 @@ const LoginScreen = () => {
           <button onClick={handleLogin}>Entrar</button>
         </div>
         <div className="button-container">
+          <button onClick={() => setShowSignupModal(true)}>Cadastrar-se</button>
           <button onClick={handleGoBack}>Voltar</button>
         </div>
       </div>
+
+      {showSignupModal && (
+        <div className="signup-modal">
+          <div className="signup-modal-content">
+            <h2>Cadastro</h2>
+            <div>
+              <label>Nome de Usuário:</label>
+              <input
+                type="text"
+                value={signupUsername}
+                onChange={(e) => setSignupUsername(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Senha:</label>
+              <input
+                type="password"
+                value={signupPassword}
+                onChange={(e) => setSignupPassword(e.target.value)}
+              />
+            </div>
+            <div>
+              <button onClick={handleSignup}>Cadastrar</button>
+              <button onClick={() => setShowSignupModal(false)}>Cancelar</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
